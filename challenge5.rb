1dde8707303563565d0c23589e09c511bcbdcb4e
Young Koh <youngheon6@gmail.com>
2023-01-06 12:45:03 -0700

    finish challenge5.rb

---
+++ challenge5.rb
@@ -0,0 +1,11 @@
+# Start with an array of travel destinations. 
+# Print every travel destination in alphabetical order embedded in a sentence using string interpolation. 
+# For example, if the destination is "New York City", print something like "The next place I want to visit is New York City!"
+#Goals: Take and array of strings and output the elemenst in that array in alphabetical order using string interpolation.
+#Pseudocode
+# 1. Assign an array of strings to a variable
+# 2. Use sort and map to iterate through array
+# 3. Output sentence using string interpolation
+
+destinations = ["Milan","Maldives","Santorini","Alaska","Cuba","Mexico","Sydney"]
+destinations.sort.map {|destination| puts "The next place I want to visit is #{destination}"}
